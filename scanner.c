f8047c441f119abda16dc19b93eccfd7793293ed
Patrik <patrik.cerbak@seznam.cz>
2022-10-24 22:54:43 +0200

    added basic file structure - makefile, scanner files and scanner tests

---
+++ scanner.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "scanner.h"
+
+void some_function() {
+    printf("testovací funkce\n");
+}
---
+++ tests_scanner.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "scanner.h"
+
+int main() {
+    printf("--- [SCANNER TESTS] ---\n");
+    some_function();
+    return 0;
+}
