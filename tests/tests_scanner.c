604b22bc8d7b33166b3cb11a3dc03656e259a401
Jiří Prokop <xproko47@stud.fit.vutbr.cz>
2022-10-31 22:14:47 +0100

    modified tests_scanner to show using scanner

--- tests/tests_scanner.c
+++ tests/tests_scanner.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "../scanner.h"
+#include "../error.h"
 
+int ret = 0;
 int main() {
     printf("--- [SCANNER TESTS] ---\n");
 
-    token_t tok = get_token();
-    printf("Tok type1 je %d'\n'", tok.type);
-    tok = get_token();
-    printf("Tok type2 je %d'\n'", tok.type);
+    token_t* tok = malloc(sizeof(token_t));
+	get_token(tok);
+    printf("Tok type1 je %d'\n'", tok->type);
+    get_token(tok);
+    printf("Tok type2 je %d'\n'", tok->type);
+    free(tok);
 
-    return 0;
+    return ret;
 }
