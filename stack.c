b7eb4abac821766f42421fe60176a1caa97d9b15
Stepan Czajkowski <stepan.m.czajkowski@gmail.com>
2022-11-18 13:53:24 +0100

    stack first implementation

---
+++ stack.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#include "scanner.h"
+#include "stack.h"
+
+
+void stack_init(stack_node_t **stack){
+    *stack = NULL;
+}
+
+int stack_push(stack_node_t **stack, token_t *token){
+    stack_node_t *tmp = malloc(sizeof(stack_node_t));
+    if(tmp == NULL){
+        return MALLOC_ERROR;
+    }
+    if(*stack == NULL){
+        tmp->next = NULL;
+    } else{
+        tmp->next = *stack;
+    }
+    tmp->current = token;
+    *stack = tmp;
+}
+
+void stack_pop(stack_node_t **stack, token_t *return_token){
+    stack_node_t *tmp;
+    if(*stack == NULL){
+        return_token = NULL;
+    }
+    tmp = *stack;
+    return_token = (*stack)->current;
+    (*stack) = (*stack)->next;
+    free(tmp);
+    tmp = NULL;
+}
+
+void stack_top(stack_node_t **stack, token_t *return_token){
+    if(*stack == NULL){
+        return_token = NULL;
+    } else{
+        return_token = (*stack)->current;
+    }
+}
+
+void stack_dispose(stack_node_t **stack){
+    stack_node_t *tmp ;
+    while(*stack != NULL){
+        tmp = *stack;
+        *stack = (*stack)->next;
+        free(tmp);
+    }
+}
+
+void stack_dispose_all(stack_node_t **stack){
+    token_t *tmp;
+    stack_node_t *tmp1;
+    while(*stack != NULL){
+        tmp = (*stack)->current;
+        tmp1 = *stack;
+        *stack = (*stack)->next;
+        free(tmp);
+        free(tmp1);
+    }
+}
