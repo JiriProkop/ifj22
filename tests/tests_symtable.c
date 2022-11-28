d63329d68030f7427ea24b54cffe2740baaa02b4
Stepan Czajkowski <stepan.m.czajkowski@gmail.com>
2022-11-28 14:28:50 +0100

    add works thanks to Peter

--- tests/tests_symtable.c
+++ tests/tests_symtable.c
@@ -30,13 +30,16 @@ void print_tree(sym_table *tree){
 }
 
 
-void add_tree_node(sym_table *tree, char id[], token_type type){
+void add_tree_node(sym_table **tree, char id[], token_type type){
     sym_data *new_symdata = malloc(sizeof(sym_data));
     dynstr_t *new_id = malloc(sizeof(dynstr_t));
+    dynstr_init(new_id);
     new_symdata->type = type;
     dynstr_add_string(new_id, id);
     printf("adding id: %s\n", new_id->array);
-    st_insert(&tree, new_id ,new_symdata);
+    printf("%p\n", *tree);
+    st_insert(tree, new_id ,new_symdata);
+
 }
 int ret = 0;
 
@@ -56,7 +59,8 @@ int main(){
     }
     tree_empty(tree);
     // test insert
-    add_tree_node(tree, "50", token_none);
+    printf("%p\n", tree);
+    add_tree_node(&tree, "50", token_none);
     tree_empty(tree);
     printf("first node added %s",tree->id->array);
     print_tree(tree);
