536c0d5a0f2fb0dc718398e0dd5f907faa80b3a8
Patrik Čerbák <cerbak01.dell@gmail.com>
2022-11-28 23:00:08 +0100

    added definition end function for now

--- src/generator.c
+++ src/generator.c
@@ -7,12 +7,15 @@ void gen_header() {
     printf(".IFJcode22\n");
 }
 
-void gen_function_def(dynstr_t *id, list_t *arguments) {
+void gen_function_def(dynstr_t *id) {
     printf("JUMP %s_end\n", id->array);
     printf("LABEL %s\n", id->array);
     printf("CREATEFRAME\n");
     printf("PUSHFRAME\n");
-    while(arguments->first != NULL) {
+}
 
-    }
+void gen_function_defend(dynstr_t *id) {
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+    printf("LABEL %d_end\n", id->array);
 }
