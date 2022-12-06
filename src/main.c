1e58b28aba18bc5aeeb34946ade81d1e75d382c7
Patrik Čerbák <cerbak01.dell@gmail.com>
2022-12-06 18:51:28 +0100

    added new makefile and main.c

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include "scanner.h"
+#include "parser.h"
+#include "generator.h"
+#include "error.h"
+#include "expr.h"
+#include "symtable.h"
+#include "dynstr.h"
+#include "stack.h"
+#include "ll.h"
+
+int main() {
+    start();
+    return ret;
+}
