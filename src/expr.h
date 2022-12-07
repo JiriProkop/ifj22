b6cc26cb6a8021711171e743209ee97c5aea82b0
Stepan Czajkowski <stepan.m.czajkowski@gmail.com>
2022-12-07 16:53:50 +0100

    merge 2 XD

--- src/expr.h
+++ src/expr.h
@@ -19,17 +19,17 @@
  */
 typedef enum {
     erule_val,				// 0:	<exp>	-> <val>
-	erule_plus,				// 1:	<exp>	-> <exp> + <exp>
-	erule_minus,			// 2:	<exp>	-> <exp> - <exp>
-	erule_div,				// 3:	<exp>	-> <exp> / <exp>
-	erule_mul,				// 4:	<exp>	-> <exp> * <exp>
+	erule_mul,				// 1:	<exp>	-> <exp> * <exp>
+	erule_div,				// 2:	<exp>	-> <exp> / <exp>
+	erule_plus,				// 3:	<exp>	-> <exp> + <exp>
+	erule_minus,			// 4:	<exp>	-> <exp> - <exp>
 	erule_cat,				// 5:	<exp>	-> <exp> . <exp>
-	erule_comp,				// 6:	<exp>	-> <exp> === <exp>
-	erule_comp_neg,			// 7:	<exp>	-> <exp> !== <exp>
-	erule_lower,			// 8:	<exp>	-> <exp> < <exp>
-	erule_lower_equal,		// 9:	<exp>	-> <exp> <= <exp>
-	erule_greater,			// 10:	<exp>	-> <exp> > <exp>
-	erule_greater_equal,	// 11:	<exp>	-> <exp> >= <exp>
+	erule_lower,			// 6:	<exp>	-> <exp> < <exp>
+	erule_lower_equal,		// 7:	<exp>	-> <exp> <= <exp>
+	erule_greater,			// 8:	<exp>	-> <exp> > <exp>
+	erule_greater_equal,	// 9:	<exp>	-> <exp> >= <exp>
+	erule_comp,				// 10:	<exp>	-> <exp> === <exp>
+	erule_comp_neg,			// 11:	<exp>	-> <exp> !== <exp>
 	erule_brackets,			// 12:	<exp>	-> ( <exp> )
 }
 expr_rules;
@@ -39,9 +39,8 @@ expr_rules;
  *
  * @param first_tok first token of expression
  * @param second_tok second token of expression, give NULL if 2nd token is unknown
- * @param symtam pointer to corresponding symtable (frame where given expressions is)
  * @return Returns false if invalid expression was found, true otherwise.
  */
-bool expr(token_t first_tok, token_t* second_tok, sym_table* symtab);
+bool expr(token_t first_tok, token_t* second_tok);
 
 #endif
