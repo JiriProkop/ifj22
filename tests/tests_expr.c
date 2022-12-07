08761b754f52712ffeec32eb56e48e7d2144c96d
Jiří Prokop <xproko47@stud.fit.vutbr.cz>
2022-12-07 13:57:06 +0100

    add local_frame to tests_expr for local var. tests

--- tests/tests_expr.c
+++ tests/tests_expr.c
@@ -14,15 +14,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TEST_EXPR_COUNT 3 // 17
+#define TEST_EXPR_COUNT 1 // 17
 
 int main() {
     token_t tok;
     token_t tok1;
     printf(".IFJcode22\nDEFVAR GF@$left_result\nDEFVAR GF@$right_result\nDEFVAR GF@$type1\nDEFVAR GF@$type2\nDEFVAR GF@$type\n");
     printf("DEFVAR GF@$tmp\nDEFVAR GF@$val1\nDEFVAR GF@$val2\nDEFVAR GF@$cond1\nDEFVAR GF@$cond2\n");
-    printf("DEFVAR GF@fuj\nDEFVAR GF@tajbl\n");
-    printf("MOVE GF@fuj int@1\nMOVE GF@tajbl int@6\n");
+    printf("CREATEFRAME\nPUSHFRAME\n");
+    printf("DEFVAR LF@fuj\nDEFVAR LF@tajbl\n");
+    printf("MOVE LF@fuj int@2\nMOVE LF@tajbl int@5\n");
     current_tkn = malloc(sizeof(token_t));
     for (int i = 0; i < TEST_EXPR_COUNT; i++) {
         get_token(&tok);
@@ -33,6 +34,6 @@ int main() {
             expr(tok, NULL);
         }
     }
-    printf("WRITE GF@$left_result\nLABEL %%TYPE_CASTING\nWRITE string@END\n");
+    printf("WRITE GF@$left_result\nLABEL %%TYPE_CASTING\nWRITE string@END\nPOPFRAME\n");
     return 0;
 }
