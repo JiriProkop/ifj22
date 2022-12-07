ccddbcc0901b736223f79f18bbcbc6eea523f5c9
Patrik Čerbák <cerbak01.dell@gmail.com>
2022-12-07 23:05:56 +0100

    removed comment

--- src/symtable.c
+++ src/symtable.c
@@ -79,7 +79,6 @@ void st_dispose(sym_table **tab) {
     if (*tab != NULL) {
         st_dispose(&(*tab)->left);
         st_dispose(&(*tab)->right);
-        //st_dispose(&(*tab)->data->local_frame); TODO
         string_free((*tab)->id);
         list_dispose((*tab)->data->parameters);
         free((*tab)->data);
